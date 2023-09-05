4e4b017a594862553844f702fc5c5f6bc36ce6c8
yj6326076 <yj6326076@hotmail.com>
2023-09-06 02:28:32 +0800

    first push

---
+++ spring-project-as-novice-main/src/test/java/com/little/main/SpringProjectAsNoviceMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.little.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectAsNoviceMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
